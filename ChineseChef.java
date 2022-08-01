82f642483af06e120602111651ef83e7a48347b3
adoumadje <adoumadjecaleb@gmail.com>
2022-08-01 17:19:12 +0300

    access modifiers

--- ChineseChef.java
+++
@@ -1,9 +0,0 @@
-public class ChineseChef {
-    public void makeSpecialDish() {
-        System.out.println("The chef makes orange chicken");
-    }
-
-    public void makeFriedRice() {
-        System.out.println("The chef makes fried rice");
-    }
-}
